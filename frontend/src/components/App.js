e50cb86d6144a2bac893482c9f45412e013ddffd
Valentin Golev <Valentin@Charlottes-MBP.fritz.box>
2019-05-24 17:07:20 +0200

    builds

---
+++ frontend/src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// import DataProvider from "./DataProvider";
+// import Table from "./Table";
+
+// const App = () => (
+//   <DataProvider endpoint="api/lead/" 
+//                 render={data => <Table data={data} />} />
+// );
+
+const App = () => <b>aaa?</b>;
+
+const wrapper = document.getElementById("app");
+wrapper ? ReactDOM.render(<App />, wrapper) : null;
